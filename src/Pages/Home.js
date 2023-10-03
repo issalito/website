02a2cd2f5640d66a49a29a59a2a741adb34e329f
issalito <issamhando@gmail.com>
2023-10-04 01:29:47 +0300

    adding resume download option

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,17 +5,35 @@ import Emailcon from "@mui/icons-material/Email";
 import "../styles/Home.css";
 import { IconButton } from "@mui/material";
 import { Link } from "react-router-dom";
+import { saveAs } from "file-saver";
 
 function Home() {
   const githubLink = "https://github.com/issalito";
   const LinkedInLink = "https://www.linkedin.com/in/issa-mhando-758523210/";
   const EmailLink = "mailto:issamhando@gmail.com";
+
+  const handleDownload = () => {
+    const resumePath = "../assets/resume.pdf";
+
+    saveAs(resumePath, "issa_mhando_Resume.pdf");
+  };
+
   return (
     <div className="home">
       <div className="about">
         <h2>Hi, My Name is Issa</h2>
         <div className="prompt">
           <p>A Computer Engineer With a Passion of Learning and Creating.</p>
+          <div>
+            <IconButton
+              variant="contained"
+              color="secondary"
+              onClick={handleDownload}
+              style={{ color: "white" }}
+            >
+              Download Resume
+            </IconButton>
+          </div>
           <IconButton component={Link} to={githubLink}>
             <GitHubIcon />
           </IconButton>

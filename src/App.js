35875c0c387f4e2d1390382a8b6a4137780389aa
issalito <issamhando@gmail.com>
2023-10-04 01:40:30 +0300

    landing page config

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // Desc: Main App component
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Projects from "./Pages/Projects";
 import Experience from "./Pages/Experience";
 import Home from "./Pages/Home";
@@ -15,7 +15,6 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/project/:id" element={<ProjectDispaly />} />
           <Route path="/experience" element={<Experience />} />
